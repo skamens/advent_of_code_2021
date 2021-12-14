359bc03467d53300a67b728d8ea8ac640ec9e7b3
Sam Kamens <snk@kam92.org>
2021-12-14 17:22:27 -0500

    Tutorial continued

--- tutorial/hello.go
+++ tutorial/hello.go
@@ -15,8 +15,11 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
 	// Request a greeting message.
-	message, err := greetings.Hello("Sam")
+	messages, err := greetings.Hellos(names)
 	// If an error was returned, print it to the console and
 	// exit the program.
 	if err != nil {
@@ -26,5 +29,5 @@ func main() {
 	// If no error was returned, print the returned message
 	// to the console.
 
-	fmt.Println(message)
+	fmt.Println(messages)
 }
