5332305375d8571f2a14deb9b82befd90c8ab6fb
Sam Kamens <snk@kam92.org>
2021-12-15 10:43:58 -0500

    Finished day 1 Yay!

--- day1/day1.go
+++ day1/day1.go
@@ -3,6 +3,7 @@ package main
 import (
 	"aoc21/aocinput"
 	"fmt"
+	"math"
 )
 
 func main() {
@@ -10,15 +11,23 @@ func main() {
 	input := "input1.txt"
 
 	vals := aocinput.LoadIntArray(input)
-	last := -1
+	last := math.MaxInt
 	increases := 0
-	for _, depth := range vals {
-		if depth > last && last != -1 {
+	for i := 0; i < len(vals)-2; i++ {
+		sum := vals[i] + vals[i+1] + vals[i+2]
+		if sum > last {
 			increases++
 		}
-
-		last = depth
+		last = sum
 	}
+
+	//for _, depth := range vals {
+	// 	if depth > last && last != -1 {
+	// 		increases++
+	// 	}
+
+	// 	last = depth
+	// }
 
 	fmt.Println(increases)
 }
