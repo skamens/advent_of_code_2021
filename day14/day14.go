bd5e5b5a579e0f6c45bb08610af5dc2baacd163a
Sam Kamens <snk@kam92.org>
2021-12-21 21:19:03 -0500

    Figured out Day 14 part 2 (with some reddit help)

--- day14/day14.go
+++ day14/day14.go
@@ -7,16 +7,6 @@ import (
 	"strings"
 )
 
-func elementCounts(template string) map[rune]int {
-	result := make(map[rune]int)
-
-	for _, r := range template {
-		result[r]++
-	}
-
-	return result
-}
-
 func main() {
 
 	input := "input14.txt"
@@ -26,6 +16,7 @@ func main() {
 	template := lines[0]
 
 	subs := make(map[string]string)
+
 	// Process rules into a map to make life easier
 	for l := 2; l < len(lines); l++ {
 		a := strings.Split(lines[l], " -> ")
@@ -35,38 +26,48 @@ func main() {
 	fmt.Println(subs)
 
 	fmt.Println(template)
-	// Gonna do 10 times through
-	for i := 0; i < 40; i++ {
-		newtemplate := ""
-		position := 0
-		matchLen := 0
-		var matchKey string
-		for position < len(template)-1 {
-			// Find the longest match in the map
-			for key, value := range subs {
-				if strings.HasPrefix(template[position:], key) {
-					if len(key) > matchLen {
-						matchKey = key
-						matchLen = len(key)
-					}
-				}
-			}
-			// So now we have the longest match
-			newtemplate.append(newtemplate, subs[key])
-		}
 
-			key := template[position : position+2]
-			template = template[0:position+1] + subs[key] + template[position+1:]
-			position += 2
-		}
-		fmt.Printf("Done with step %d; template length %d\n", i+1, len(template))
+	pairCounts := make(map[string]int)
+	// Start with the initial template
+	for position := 0; position < len(template)-1; position++ {
+		pair := template[position : position+2]
+		pairCounts[pair]++
 	}
 
-	counts := elementCounts(template)
+	letterCounts := make(map[rune]int)
+	// initialize the letter counts
+	for _, r := range template {
+		letterCounts[r]++
+	}
+
+	// Gonna do 10 times through
+	for i := 0; i < 40; i++ {
+		// Now go through all the pairs and change counts appropriately
+		newPairCounts := make(map[string]int)
+
+		for pair := range pairCounts {
+			// For each pair, add and remove an appopriate number of pairs
+			// in newPairCounts
+
+			newPairCounts[pair] -= pairCounts[pair]
+			newPairCounts[pair[0:1]+subs[pair]] += pairCounts[pair]
+			newPairCounts[subs[pair]+pair[1:2]] += pairCounts[pair]
+
+			for _, r := range subs[pair] {
+				letterCounts[r] += pairCounts[pair]
+			}
+		}
+
+		for pair := range newPairCounts {
+			pairCounts[pair] += newPairCounts[pair]
+		}
+
+		fmt.Println(pairCounts)
+	}
 
 	highest := 0
 	lowest := math.MaxInt
-	for _, c := range counts {
+	for _, c := range letterCounts {
 		if c > highest {
 			highest = c
 		}
