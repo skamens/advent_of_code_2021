af4ba034da688a235622b0c9f06dd236722e96ec
Sam Kamens <snk@kam92.org>
2021-12-23 12:07:12 -0500

    Day17 complete

--- day17/day17.go
+++ day17/day17.go
@@ -7,9 +7,44 @@ import (
 	"strings"
 )
 
+type probe struct {
+	xpos int
+	ypos int
+	xvel int
+	yvel int
+
+	highesty int
+}
+
+func step(p *probe, minx int, maxx int, miny int, maxy int) bool {
+	p.xpos += p.xvel
+	p.ypos += p.yvel
+
+	if p.ypos > p.highesty {
+		p.highesty = p.ypos
+	}
+
+	if p.xvel > 0 {
+		p.xvel--
+	} else if p.xvel < 0 {
+		p.xvel++
+	}
+
+	p.yvel--
+
+	// OK, now is it in the target area. X and Y must both be within
+	// the ranges
+
+	if p.xpos >= minx && p.xpos <= maxx && p.ypos >= miny && p.ypos <= maxy {
+		return true
+	} else {
+		return false
+	}
+}
+
 func main() {
 
-	input := "test17.txt"
+	input := "input17.txt"
 
 	target := aocutil.LoadSingleString(input)
 
@@ -28,5 +63,44 @@ func main() {
 	miny, _ := strconv.Atoi(yarr[0])
 	maxy, _ := strconv.Atoi(yarr[1])
 
-	fmt.Printf("minx: %d, maxx: %d, miny: %d, maxy: %d\n", minx, maxx, miny, maxy)
+	highesty := 0
+
+	points := make(map[aocutil.Point]int)
+
+	for initialy := -500; initialy < 500; initialy++ {
+		fmt.Printf("initialy=%d\n", initialy)
+		//anysuccess := false
+
+		for initialx := 0; initialx < 500; initialx++ {
+			//fmt.Printf("   initialx=%d ... ", initialx)
+
+			var p = probe{xpos: 0, ypos: 0, xvel: initialx, yvel: initialy, highesty: 0}
+
+			//success := false
+			for p.xpos <= maxx && p.ypos >= miny {
+				if step(&p, minx, maxx, miny, maxy) {
+					//anysuccess = true
+					//success = true
+
+					points[aocutil.Point{X: initialx, Y: initialy}]++
+					//fmt.Println(points)
+
+					if p.highesty > highesty {
+						fmt.Printf("Highest y = %d, initialy = %d\n", p.highesty, initialy)
+						highesty = p.highesty
+					}
+					break
+				}
+			}
+			//fmt.Println(success)
+		}
+		//if !anysuccess {
+		//	break
+		//}
+
+	}
+
+	fmt.Printf("Done. highest y is at %d\n", highesty)
+	fmt.Printf("Total velocities: %d\n", len(points))
+
 }
